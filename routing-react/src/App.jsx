ba71a1838f9b0fc352f04766b1b3ba98060cd97d
natia <karamnishvili.natia@gmail.com>
2026-07-13 15:09:55 +0400

    add last changes

--- routing-react/src/App.jsx
+++ routing-react/src/App.jsx
@@ -1,22 +1,10 @@
-import Header from "./components/Header";
-import { Routes, Route } from "react-router-dom";
-import About from "./pages/About";
-import Home from "./pages/Home";
-import OurPosts from "./pages/OurPosts";
-import Contact from './pages/Contact'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return <>
   
 
-  <Header></Header>
-  <Routes>
-    <Route path='/' element={<Home></Home>}></Route>
-    <Route path="/about" element={<About></About>}></Route>
-    <Route path="/about/ourPosts" element={<OurPosts/>}></Route>
-    <Route path="/contact" element={<Contact></Contact>}></Route>
-
-  </Routes>
+Routes
   
 
   </>;
