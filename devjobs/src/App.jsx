76a26fb4345066f5e16f95543a35b908c5516e65
natia karamnishvili <naka@athena.com>
2026-07-14 22:48:54 +0400

    fix:router syntax error

--- devjobs/src/App.jsx
+++ devjobs/src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Jobs from "./pages/Jobs";
 import JobDetails from "./pages/JobDetails";
-import { Router, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <Router>
+      <Routes>
         <Route path="/" element={<Jobs></Jobs>}></Route>
         <Route path="/jobdetails" element={<JobDetails></JobDetails>}></Route>
-      </Router>
+      </Routes>
     </>
   );
 }
