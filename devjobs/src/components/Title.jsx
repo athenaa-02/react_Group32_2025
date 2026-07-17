5c52e853ad82c236c8cfaf15e6d1c925c19c2306
natia <karamnishvili.natia@gmail.com>
2026-07-17 15:22:40 +0400

    use regex to extract domain from url

--- devjobs/src/components/Title.jsx
+++ devjobs/src/components/Title.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import DomainLabel from "./DomainLabel";
 
-function Title() {
+function Title(props) {
+
+
   return (
-    <div>Title</div>
-  )
+    <>
+      <div className="flex flex-col ">
+        <div>
+          <div className="h-12.5 w-12.5 flex items-center rounded-[15px]" style={{backgroundColor:props.color}}>
+          <img className="w-full"  src={props.img} alt="" />
+       </div>
+        </div>
+        <div>
+          <h3>{props.company}</h3>
+          <DomainLabel url={props.webLink}></DomainLabel>
+        </div>
+        <a href="">Company Site</a>
+      </div>
+    </>
+  );
 }
 
-export default Title
+export default Title;
--- devjobs/src/pages/JobDetails.jsx
+++ devjobs/src/pages/JobDetails.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-
-
+import React from "react";
+import jobs from "../data/data";
+import Title from "../components/Title";
+import { useParams } from "react-router-dom";
 
 function JobDetails() {
-const {id} = useParams('/:id')
-console.log(id)
+  const { id } = useParams("/:id");
+
+  console.log(jobs[0].id);
+  const selectedJob = jobs.find((job) => {
+    return job.id === Number(id);
+  });
+  console.log(selectedJob);
+
+  const {
+    logo,
+    company,
+    logoBackground,
+    position,
+    postedAt,
+    requirements,
+    role,
+    website,
+    location,
+    apply,
+    contract,
+    description,
+  } = selectedJob;
 
   return (
-   <>
-   <h1>djhf</h1>
-   </>
-  )
+    <>
+      <Title img={logo} company={company} webLink={website} color={logoBackground}></Title>
+    </>
+  );
 }
 
-export default JobDetails
+export default JobDetails;
